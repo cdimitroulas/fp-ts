138164481e55532ed79fc6f4c32987ad2ee64fdc
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-04-01 10:52:33 +0100

    Improve structure

--- dtslint/ts3.5/struct.ts
+++ dtslint/ts3.5/struct.ts
@@ -1,21 +1,20 @@
 import * as _ from '../../src/struct'
 import { pipe } from '../../src/function'
 
-declare const p1: {
-  k1: string
-  k2: number
-}
+declare const s1: { k1: string, k2: number }
+declare const s2: { k1: number }
 
-declare const p2: {
-  k1: number
-}
+//
+// prop
+//
 
 const getK1 = _.prop('k1')
 const getFoo = _.prop('foo')
 
-pipe(p1, _.prop('k1')) // $ExpectType string
-pipe(p2, _.prop('k1')) // $ExpectType number
-pipe(p1, _.prop('foo')) // $ExpectError
-pipe(p1, getK1) // $ExpectType string
-pipe(p2, getK1) // $ExpectType number
-pipe(p1, getFoo) // $ExpectError
+pipe(s1, _.prop('k1')) // $ExpectType string
+pipe(s1, _.prop('k2')) // $ExpectType number
+pipe(s2, _.prop('k1')) // $ExpectType number
+pipe(s1, _.prop('foo')) // $ExpectError
+pipe(s1, getK1) // $ExpectType string
+pipe(s2, getK1) // $ExpectType number
+pipe(s1, getFoo) // $ExpectError
